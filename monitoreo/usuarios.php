44836a39844395a49e35ed5a2c1d6a153790b295
KFRANCGIT <kmadrigal@ucol.mx>
2017-12-02 18:19:56 -0600

    .

--- monitoreo/usuarios.php
+++ monitoreo/usuarios.php
@@ -33,6 +33,7 @@
 								<li><a href="arduinos.php">Arduinos</a></li>
 								<li><a href="mediciones.php">Mediciones</a></li>
 								<li><a href="alarmas.php">Alarmas</a></li>
+								<li><a href="ubicacion/Ubicacion.php">Alarmas</a></li>
 							</ul>
 						</li>
 					</ul>

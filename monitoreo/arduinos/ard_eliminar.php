ca163b83722fff600e6425310eb6d5b1f96b175e
KFRANCGIT <kmadrigal@ucol.mx>
2017-12-03 12:35:06 -0600

    Agregado_configuracion_arduinos

--- monitoreo/arduinos/ard_eliminar.php
+++ monitoreo/arduinos/ard_eliminar.php
@@ -5,11 +5,11 @@
   		$conn = new mysqli('localhost', 'simp', 'simpcolima', 'SIMP2');
 
   		if(!$conn->connect_error){
-  				$sql = "delete from arduinos where id = ".$id_arduinos;
+  				$sql = "delete from arduinos where id_arduinos = ".$id_arduinos;
   				$sql_respuesta = $conn->query($sql);
   				if($sql_respuesta)
           {
-            $response =  array('done' => true, 'message' => 'Se eliminó la alarma');
+            $response =  array('done' => true, 'message' => 'Se eliminó la estación');
           }
 
 

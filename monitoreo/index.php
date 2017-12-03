dfbe5a8668284af9001186ab6abc6860cb31ae6a
KFRANCGIT <kmadrigal@ucol.mx>
2017-12-02 20:54:38 -0600

    .

--- monitoreo/index.php
+++ monitoreo/index.php
@@ -514,110 +514,15 @@
 												<!-- /input-group -->
 										</li>
 										<li>
-												<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+												<a href="index.php"><i class="fa fa-home fa-fw"></i> Inicio</a>
 										</li>
 										<li>
-												<a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
-												<ul class="nav nav-second-level">
-														<li>
-																<a href="flot.html">Flot Charts</a>
-														</li>
-														<li>
-																<a href="morris.html">Morris.js Charts</a>
-														</li>
-												</ul>
-												<!-- /.nav-second-level -->
+												<a href="#"><i class="fa fa-location-arrow fa-fw"></i> Ubicación de Estaciones<span class="fa arrow"></span></a>
 										</li>
 										<li>
 												<a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
 										</li>
-										<li>
-												<a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
-										</li>
-										<li>
-												<a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-												<ul class="nav nav-second-level">
-														<li>
-																<a href="panels-wells.html">Panels and Wells</a>
-														</li>
-														<li>
-																<a href="buttons.html">Buttons</a>
-														</li>
-														<li>
-																<a href="notifications.html">Notifications</a>
-														</li>
-														<li>
-																<a href="typography.html">Typography</a>
-														</li>
-														<li>
-																<a href="icons.html"> Icons</a>
-														</li>
-														<li>
-																<a href="grid.html">Grid</a>
-														</li>
-												</ul>
-												<!-- /.nav-second-level -->
-										</li>
-										<li>
-												<a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-												<ul class="nav nav-second-level">
-														<li>
-																<a href="#">Second Level Item</a>
-														</li>
-														<li>
-																<a href="#">Second Level Item</a>
-														</li>
-														<li>
-																<a href="#">Third Level <span class="fa arrow"></span></a>
-																<ul class="nav nav-third-level">
-																		<li>
-																				<a href="#">Third Level Item</a>
-																		</li>
-																		<li>
-																				<a href="#">Third Level Item</a>
-																		</li>
-																		<li>
-																				<a href="#">Third Level Item</a>
-																		</li>
-																		<li>
-																				<a href="#">Third Level Item</a>
-																		</li>
-																</ul>
-																<!-- /.nav-third-level -->
-														</li>
-												</ul>
-												<!-- /.nav-second-level -->
-										</li>
-										<li>
-												<a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-												<ul class="nav nav-second-level">
-														<li>
-																<a href="blank.html">Blank Page</a>
-														</li>
-														<li>
-																<a href="login.html">Login Page</a>
-														</li>
-												</ul>
-												<!-- /.nav-second-level -->
-										</li>
-										<main>
-											<!--_________________________________________________UNA___________________________________________________________________-->
-
-
-											<!--_______________________________________VISOR MEDICIONES___________________________________________________-->
-											<?php
-											 /*$conn = mysqli_connect("localhost","root","","SIMP2");
-											 //$query ="SELECT nombre_arduino,nombre_sensor,ubicacion,activo,valor,unidad,fecha FROM mediciones join (sensores,arduinos) on (sensores.id_sensores);";
-											 $query ="SELECT * FROM mediciones ORDER BY `mediciones`.`id_mediciones` DESC LIMIT 4";
-											 //$quer = "SELECT * FROM `$tabla` WHERE `$iden` = '$id'";
-											 //$query ="SELECT * FROM mediciones ORDER BY id_mediciones DESC";
-
-
-											 $result = mysqli_query($conn, $query)
-											 or die("Error: ".mysqli_error($conn));
-											*/?>
-
-
+								
 								</ul>
 
 						</div>

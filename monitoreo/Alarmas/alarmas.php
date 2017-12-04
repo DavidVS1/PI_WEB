90461935abf0b40cfeda5ef957f0c47994263929
KFRANCGIT <kmadrigal@ucol.mx>
2017-12-04 02:08:44 -0600

    .

--- monitoreo/Alarmas/alarmas.php
+++ monitoreo/Alarmas/alarmas.php
@@ -58,7 +58,7 @@
 									</li>
 								</ul>
 								<ul class="nav navbar-nav navbar-right">
-									<li><a href="../cerrar_sesion.php"><i class="glyphicon glyphicon-log-out"></i>  Cerrar Sesión</a></li>
+									<li><a href="../../cerrar_sesion.php"><i class="glyphicon glyphicon-log-out"></i>  Cerrar Sesión</a></li>
 								</ul>
 							</div>
 						</nav>
